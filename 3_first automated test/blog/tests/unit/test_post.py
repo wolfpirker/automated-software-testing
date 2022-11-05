e83c1cb859cc56b8ae9dce84c5bf7b1306b7f6de
Wolfgang Pirker <w_pirker@gmx.de>
2022-11-06 00:24:45 +0100

    a first automated test in Python; Unit Tests, part 1

---
+++ 3_first automated test/blog/tests/unit/test_post.py
@@ -0,0 +1,16 @@
+from unittest import TestCase
+from post import Post
+
+class TestPost(TestCase):
+    def test_create_post(self):
+        p = Post('Test', 'Test content')
+
+        self.assertEqual('Test', p.title)
+        self.assertEqual('Test content', p.content)
+
+    def test_json(self):
+        p = Post('Test', 'Test content')
+        expected = {'title': p.title, 'content': p.content}
+        self.assertDictEqual(expected, p.json())
+
+
