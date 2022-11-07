ea030eb88c3bb3c3fdeea992e35d93210ade8459
Wolfgang Pirker <w_pirker@gmx.de>
2022-11-07 19:15:46 +0100

    create a console app, with system tests and patching pt. 2

--- 3_first automated test/blog/app.py
+++ 3_first automated test/blog/app.py
@@ -1,6 +1,5 @@
 from blog import Blog
 
-
 MENU_PROMPT = '\nEnter "c" to create a blog, "l" to list them, "r" to read one, "p" to write a post, or "q" to quit: '
 POST_TEMPLATE = """
 --- {} ---
@@ -13,14 +12,62 @@ blogs = dict()
 
 
 def show_menu():
+    # show the user the available blogs
+    # let the user make a choice
+    # exit on request
     print_blogs()
     selection = input(MENU_PROMPT)
+    while selection != 'q':
+        if selection == 'c':
+            ask_create_blog()
+        elif selection == 'l':
+            print_blogs()
+        elif selection == 'r':
+            ask_read_blog()
+        elif selection == 'p':
+            ask_create_post()
+        selection = input(MENU_PROMPT)
 
+
+def ask_create_blog():
+    """ function to create basic blog entries"""
+    # Note: this function ended up quite a bit complex;
+    # course teacher suggestion: start simple and create a working test;
+    # then extend gradually , and test in between
+    title = str(input("enter a title as blog title: \n"))
+    if (len(title)) <= 3:
+        print('blog title must contain more than 3 letters')
+        return
+    author = str(input("enter an author or keep blank: \n"))
+    b = Blog(title, author)
+    # Note: instead just create blog without posts!
+    # while True:
+    #     # let user create several blog posts
+    #     print('create a new post')
+    #     title_pos = input("enter the post title (or leave blank to skip): \n")
+    #     if len(title_pos) <= 3:
+    #         print('skip')
+    #         break
+    #     else:
+    #         content = input("enter the content: \n")
+    #         b.create_post(title, content)
+    answer = input("save blog y/n (y): ")
+    if answer != 'n':
+        blogs[b.title] = b
+        print('blog created')
+    else:
+        print('blog entry canceled')
 
 
 def print_blogs():
     for key, blog in blogs.items():
         print('- {}'.format(blog))
+
+
+def ask_read_blog():
+    title = input("Enter the blog title you want to read:\n")
+    # Note: try/catch not required, we keep it simplistic
+    print_posts(blogs[title])
 
 
 def print_posts(blog):
@@ -30,6 +77,18 @@ def print_posts(blog):
 
 def print_post(post):
     print(POST_TEMPLATE.format(post.title, post.content))
+
+
+def ask_create_post():
+    """basic function to create a single post for a blog"""
+    blog = input("Enter the blog title you want to create a post in: ")
+    b = blogs[blog]
+    title = input("enter the post title (or leave blank to cancel): \n")
+    if len(title) <= 3:
+        print('cancel create post')
+    else:
+        content = input("enter the content: \n")
+        b.create_post(title, content)
 
 
 if __name__ == '__main__':
--- 3_first automated test/blog/tests/system/test_app.py
+++ 3_first automated test/blog/tests/system/test_app.py
@@ -24,24 +24,65 @@ class AppTest(TestCase):
                 mocked_print.assert_called_with('- Test by Test Author (0 posts)')
 
     def test_menu_prints_prompt(self):
-        with patch('builtins.input') as mocked_input:
+        with patch('builtins.input', return_value='q') as mocked_input:
             app.show_menu()
             mocked_input.assert_called_with(app.MENU_PROMPT)
 
     def test_menu_calls_print_blogs(self):
         with patch('builtins.input') as mocked_input:
             with patch('app.print_blogs') as mocked_print_blogs:
+                mocked_input.side_effect = ('l', 'q')
                 app.show_menu()
                 mocked_print_blogs.assert_called()
+
+    def test_ask_create_blog(self):
+        with patch('builtins.input') as mocked_input:
+            mocked_input.side_effect = ('Test 2', 'Test Author 2', 'y')
+            app.ask_create_blog()
+            # Note: following assertion does not result equal
+            # surely because it is not the same object reference!
+            # self.assertEqual(app.blogs['Test 2'], 'Test 2 by Test Author 2 (0 posts)')
+
+            # so check whether fields are equal instead!
+            self.assertIsNotNone(app.blogs.get('Test 2')) # rather not forget that!
+            self.assertEqual((app.blogs['Test 2'].title), 'Test 2')
+            self.assertEqual((app.blogs['Test 2'].author), 'Test Author 2')
 
     def test_print_blogs(self):
         with patch('builtins.print') as mocked_print:
             app.print_blogs()
             mocked_print.assert_called_with('- Test by Test Author (0 posts)')
 
+    def test_ask_read_blog(self):
+        with patch('builtins.input', return_value='Test'):
+            with patch('app.print_posts') as mocked_print_posts:
+                app.ask_read_blog()
+                mocked_print_posts.assert_called_with(app.blogs['Test'])
+
+            # initial mistake in own attempt:
+            # Note: patching builtins.print would be awkward;
+            # better patch app.print_posts!
+            # with patch('builtins.print') as mocked_print:
+                # wrong! it is not about printing a single post
+                # in this test we should call app.ask_read_blog()!
+                # app.print_post(app.blogs['Test'])
+                # mocked_print.assert_called_with(app.blogs['Test'])
+
+
+
+
+    def test_ask_create_post(self):
+        # app.ask_create_post() -> not here, but after our side effects!
+        blog = app.blogs['Test']
+        with patch('builtins.input') as mocked_input:
+            mocked_input.side_effect = ('Test', 'Post Title', 'Test Content')
+            app.ask_create_post()
+            self.assertEqual(blog.posts[0].title, 'Post Title')
+            self.assertEqual(blog.posts[0].content, 'Test Content')
+
+
     def test_print_post(self):
         post = Post('Post title', 'Post content')
-
         # Note:
         # better it would be actually to not rely on POST_TEMPLATE
         # probably better to copy and paste the TEMPLATE here
