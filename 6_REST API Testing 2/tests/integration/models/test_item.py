d0d85f5bd2c902708dc3f81d649d23bc7cf81fad
Wolfgang Pirker <w_pirker@gmx.de>
2022-12-13 21:47:35 +0100

    integration test fixes

--- 6_REST API Testing 2/tests/integration/models/test_item.py
+++ 6_REST API Testing 2/tests/integration/models/test_item.py
@@ -1,11 +1,16 @@
 from models.item import ItemModel
+from models.store import StoreModel
 from tests.base_test import BaseTest
 
 
 class ItemTest(BaseTest):
     def test_crud(self):
         with self.app_context():
-            item = ItemModel('test', 19.99)
+            StoreModel('test').save_to_db() # recommended to do that
+            # so it works with Postgres, MySQL etc.
+            item = ItemModel('test', 19.99, 1)
+            # Note issue: in certain SQL databases
+            # it should be required
 
             self.assertIsNone(ItemModel.find_by_name('test'),
                               "Found an item with name {}, but expected not to.".format(item.name))
--- 6_REST API Testing 2/tests/unit/models/test_item.py
+++ 6_REST API Testing 2/tests/unit/models/test_item.py
@@ -5,7 +5,7 @@ from models.item import ItemModel
 
 class ItemTest(TestCase):
     def test_create_item(self):
-        item = ItemModel('test', 19.99)
+        item = ItemModel('test', 19.99, 1)
 
         self.assertEqual(item.name, 'test',
                          "The name of the item after creation does not equal the constructor argument.")
@@ -13,7 +13,7 @@ class ItemTest(TestCase):
                          "The price of the item after creation does not equal the constructor argument.")
 
     def test_item_json(self):
-        item = ItemModel('test', 19.99)
+        item = ItemModel('test', 19.99, 1)
         expected = {
             'name': 'test',
             'price': 19.99
